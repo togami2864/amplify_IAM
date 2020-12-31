4d0c6d19569d76ac36b9235e895d500d6a7233ae
Yoshiaki Togami <tuabobo123@gmail.com>
2020-12-31 15:43:14 +0900

    initial-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import API from "@aws-amplify/api";
+import * as mutations from "./graphql/mutations";
+import * as queries from "./graphql/queries";
+import { GRAPHQL_AUTH_MODE } from "@aws-amplify/api-graphql/lib/types";
 
 function App() {
+  const todoDetails = {
+    name: "test",
+    description: "test",
+  };
+  const generateTodo = async () => {
+    try {
+      await API.graphql({
+        query: mutations.createTodo,
+        variables: { input: todoDetails },
+        authMode: GRAPHQL_AUTH_MODE.AWS_IAM,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const getList = async () => {
+    try {
+      console.log(
+        await API.graphql({
+          query: queries.listTodos,
+          authMode: GRAPHQL_AUTH_MODE.AWS_IAM,
+        })
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={getList}>Query</button>
+      <button onClick={generateTodo}>Mutation</button>
+    </>
   );
 }
 
---
+++ src/graphql/mutations.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createTodo = /* GraphQL */ `
+  mutation CreateTodo(
+    $input: CreateTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    createTodo(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
